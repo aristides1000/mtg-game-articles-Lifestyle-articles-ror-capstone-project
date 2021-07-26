3a65af2cca45ba44d9f157e6e2aa6c1982bfbf60
aristides1000 <aristides1000@gmail.com>
2021-07-26 14:55:53 -0400

    fix some redirect_to

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,7 +27,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: "Category was successfully created." }
+        format.html { redirect_to root_path, notice: "Category was successfully created." }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
