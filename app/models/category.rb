0387fd43cb8e7651a2ad44ca5609d70b9e344f47
aristides1000 <aristides1000@gmail.com>
2021-07-27 12:51:08 -0400

    Comments code in the application removed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,4 @@
 class Category < ApplicationRecord
-  #   has_many :articles, foreign_key: :articleid, dependent: :destroy
-  #   has_many :article_categories, foreign_key: :categoryid, dependent: :destroy
   has_many :article_categories, foreign_key: :category_id
   has_many :articles, through: :article_categories
 
